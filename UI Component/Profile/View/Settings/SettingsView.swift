797a7e7a605ffe27c4498b57fbe2bb0d931d72f1
yu.he <yu.he@thoughtworks.com>
2022-09-21 17:02:41 +0800

    fix settingView

--- UI Component/Profile/View/Settings/SettingsView.swift
+++ UI Component/Profile/View/Settings/SettingsView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct SettingsView: View {
+    let topId: String="topId"
+    let item = ListItemModel(icon: "gearshape", title: "设置")
     var body: some View {
-        Text("SettingsView")
+        ScrollViewReader { proxy in
+            List{
+                Text("Settings Page")
+                    .id(topId)
+                ForEach(0..<20) {_ in
+                    HStack {
+                        Image(systemName: item.icon)
+                        Text(item.title)
+                    }
+                }
+                Button("Back To Top") {
+                    withAnimation(.spring()){
+                        proxy.scrollTo(topId, anchor: .top)
+                    }
+                }
+            }
+        }
     }
 }
 
